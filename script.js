14cd5af008b798b9a6f30489e4722d88153aff2e
arguablykomodo <arguablykomodo@users.noreply.github.com>
2022-05-26 00:52:59 +0000

    deploy: ea34ff5774c2705c800d8e7ad7d246ebe7b9bb1c

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 /** @type {HTMLInputElement} */
+const fileRadio = document.getElementById("fileRadio");
+/** @type {HTMLInputElement} */
 const fileInput = document.getElementById("file");
+/** @type {HTMLInputElement} */
+const urlRadio = document.getElementById("urlRadio");
+/** @type {HTMLInputElement} */
+const urlInput = document.getElementById("url");
 /** @type {HTMLInputElement} */
 const speedOverrideInput = document.getElementById("speedOverride");
 /** @type {HTMLInputElement} */
@@ -25,22 +31,36 @@ const shuffledImg = document.getElementById("shuffled");
 let imageData;
 
 async function loadFile() {
-  const file = fileInput.files?.[0];
-  if (file) {
-    const buffer = await file.arrayBuffer();
-    imageData = new Uint8Array(buffer);
-    originalImg.src = URL.createObjectURL(file);
+  if (fileRadio.checked) {
+    const file = fileInput.files?.[0];
+    if (file) {
+      const buffer = await file.arrayBuffer();
+      imageData = new Uint8Array(buffer);
+      originalImg.src = URL.createObjectURL(file);
+      originalFigure.classList.remove("hidden");
+      shuffledFigure.classList.add("hidden");
+    } else {
+      originalFigure.classList.add("hidden");
+    }
+  } else if (urlRadio.checked) {
+    const response = await fetch(urlInput.value);
+    const blob = await response.blob();
+    imageData = new Uint8Array(await blob.arrayBuffer());
+    originalImg.src = URL.createObjectURL(blob);
     originalFigure.classList.remove("hidden");
     shuffledFigure.classList.add("hidden");
-  } else {
-    originalFigure.classList.add("hidden");
   }
 }
 
 loadFile();
+fileRadio.addEventListener("change", loadFile);
 fileInput.addEventListener("change", loadFile);
+urlRadio.addEventListener("change", loadFile);
+urlInput.addEventListener("change", loadFile);
 
-if (seedInput.value === "") seedInput.value = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+if (seedInput.value === "") {
+  seedInput.value = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+}
 
 speedInput.disabled = !speedOverrideInput.checked;
 speedOverrideInput.addEventListener("change", () => {
@@ -63,7 +83,9 @@ const { instance: { exports } } = await WebAssembly.instantiateStreaming(
  */
 function ret(ptr, len) {
   const buffer = new Uint8Array(exports.memory.buffer, ptr, len);
-  shuffledImg.src = URL.createObjectURL(new Blob([buffer], { type: "image/gif" }));
+  shuffledImg.src = URL.createObjectURL(
+    new Blob([buffer], { type: "image/gif" }),
+  );
   shuffledFigure.classList.remove("hidden");
   exports.free(ptr, len);
 }
@@ -80,29 +102,35 @@ const errors = {
 
 shuffleButton.addEventListener("click", async () => {
   if (!imageData) alert("Please upload a file");
-  else try {
-    /** @type {number} */
-    const ptr = exports.alloc(imageData.length);
-    const buffer = new Uint8Array(exports.memory.buffer, ptr, imageData.length);
-    buffer.set(imageData);
-    /** @type {number} */
-    const result = exports.main(
-      ptr,
-      imageData.length,
-      BigInt(seedInput.value),
-      speedOverrideInput.checked,
-      speedInput.valueAsNumber / 10,
-      loopOverrideInput.checked,
-      loopInput.valueAsNumber,
-    );
-    if (result !== 0) {
-      const keyBuffer = new Uint8Array(exports.memory.buffer, result, 25);
-      let i = 0;
-      while (keyBuffer[i] !== 0) i += 1;
-      const key = new TextDecoder().decode(keyBuffer.slice(0, i));
-      alert(errors[key]);
+  else {
+    try {
+      /** @type {number} */
+      const ptr = exports.alloc(imageData.length);
+      const buffer = new Uint8Array(
+        exports.memory.buffer,
+        ptr,
+        imageData.length,
+      );
+      buffer.set(imageData);
+      /** @type {number} */
+      const result = exports.main(
+        ptr,
+        imageData.length,
+        BigInt(seedInput.value),
+        speedOverrideInput.checked,
+        speedInput.valueAsNumber / 10,
+        loopOverrideInput.checked,
+        loopInput.valueAsNumber,
+      );
+      if (result !== 0) {
+        const keyBuffer = new Uint8Array(exports.memory.buffer, result, 25);
+        let i = 0;
+        while (keyBuffer[i] !== 0) i += 1;
+        const key = new TextDecoder().decode(keyBuffer.slice(0, i));
+        alert(errors[key]);
+      }
+    } catch (e) {
+      alert(e);
     }
-  } catch (e) {
-    alert(e);
   }
 });
