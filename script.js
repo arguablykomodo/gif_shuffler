4597510a5cdba9ba55db0242971cac0d404fb232
Komodo <noreply@komodo.ar>
2022-05-25 21:48:02 -0300

    style: format code

--- script.js
+++ script.js
@@ -58,7 +58,9 @@ fileInput.addEventListener("change", loadFile);
 urlRadio.addEventListener("change", loadFile);
 urlInput.addEventListener("change", loadFile);
 
-if (seedInput.value === "") seedInput.value = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+if (seedInput.value === "") {
+  seedInput.value = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+}
 
 speedInput.disabled = !speedOverrideInput.checked;
 speedOverrideInput.addEventListener("change", () => {
@@ -81,7 +83,9 @@ const { instance: { exports } } = await WebAssembly.instantiateStreaming(
  */
 function ret(ptr, len) {
   const buffer = new Uint8Array(exports.memory.buffer, ptr, len);
-  shuffledImg.src = URL.createObjectURL(new Blob([buffer], { type: "image/gif" }));
+  shuffledImg.src = URL.createObjectURL(
+    new Blob([buffer], { type: "image/gif" }),
+  );
   shuffledFigure.classList.remove("hidden");
   exports.free(ptr, len);
 }
@@ -98,29 +102,35 @@ const errors = {
 
 shuffleButton.addEventListener("click", async () => {
   if (!imageData) alert("Please upload a file");
-  else try {
-    /** @type {number} */
-    const ptr = exports.alloc(imageData.length);
-    const buffer = new Uint8Array(exports.memory.buffer, ptr, imageData.length);
-    buffer.set(imageData);
-    /** @type {number} */
-    const result = exports.main(
-      ptr,
-      imageData.length,
-      BigInt(seedInput.value),
-      speedOverrideInput.checked,
-      speedInput.valueAsNumber / 10,
-      loopOverrideInput.checked,
-      loopInput.valueAsNumber,
-    );
-    if (result !== 0) {
-      const keyBuffer = new Uint8Array(exports.memory.buffer, result, 25);
-      let i = 0;
-      while (keyBuffer[i] !== 0) i += 1;
-      const key = new TextDecoder().decode(keyBuffer.slice(0, i));
-      alert(errors[key]);
+  else {
+    try {
+      /** @type {number} */
+      const ptr = exports.alloc(imageData.length);
+      const buffer = new Uint8Array(
+        exports.memory.buffer,
+        ptr,
+        imageData.length,
+      );
+      buffer.set(imageData);
+      /** @type {number} */
+      const result = exports.main(
+        ptr,
+        imageData.length,
+        BigInt(seedInput.value),
+        speedOverrideInput.checked,
+        speedInput.valueAsNumber / 10,
+        loopOverrideInput.checked,
+        loopInput.valueAsNumber,
+      );
+      if (result !== 0) {
+        const keyBuffer = new Uint8Array(exports.memory.buffer, result, 25);
+        let i = 0;
+        while (keyBuffer[i] !== 0) i += 1;
+        const key = new TextDecoder().decode(keyBuffer.slice(0, i));
+        alert(errors[key]);
+      }
+    } catch (e) {
+      alert(e);
     }
-  } catch (e) {
-    alert(e);
   }
 });
